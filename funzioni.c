37ed0155b65b2416aa454956852acd4efd513ad4
Paolo Rampino <liadserv@gmail.com>
2024-11-12 08:17:32 -0500

    up

--- funzioni.c
+++ funzioni.c
@@ -6,7 +6,13 @@ int somma(int a, int b){
     return c;
 }
 
+float divisione(int a, int b){
+    if (b == 0) return 0.0;
+    return (float)a/(float)b;
+}
+
 int main(){
     printf("\nSomma: %d", somma(3, 4));
+    printf("\nDiviso: %2.f", divisione(3, 4));
     return 0;
 }
