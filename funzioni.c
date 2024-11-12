81306eded3fcef69cabb38430a74789e3d45ac45
Paolo Rampino <liadserv@gmail.com>
2024-11-12 08:41:40 -0500

    up

--- funzioni.c
+++ funzioni.c
@@ -14,7 +14,7 @@ float divisione(int a, int b){
 int main(){
     char nome[] = "Caio";
     printf("\nSomma: %d", somma(3, 4));
-    printf("\nDiviso: %f", divisione(3, 4));
+    printf("\nDiviso: %.2f", divisione(3, 4));
     printf("\nStringa %s", nome);
     return 0;
 }
