1ae0fc782285215bf6947d862d86af53ac561888
Paolo Rampino <liadserv@gmail.com>
2024-11-13 04:25:17 -0500

    up

---
+++ matematicaBase.py
@@ -0,0 +1,4 @@
+a=30
+b=70
+print(f"{a} + {b} = {a+b}")
+print(f"{a} - {b} = {a-b}")
---
+++ variabili.py
@@ -0,0 +1,28 @@
+# variabili
+# devono iniziare per a-z o A-Z o _
+A2382="Pippo"
+art231="Pippo2"
+_var="Pippo"
+_var=2
+print(_var)
+# non usate parole chiave con if, else, for, print etc
+# quindi if="ciao" non funziona!
+# convenzioni delle varibili
+# le varibili, per convenzione, sono sempre composte da lettere minuscole e underscore
+variabile_uno=1
+il_mio_nome=1
+# mentre le classi sono espresse con lettere maiuscole iniziali e in camelCase
+# LaMiaClasse
+#Tipi di variabili
+x = 10         # intero
+y = 3.14       # float
+nome = "Alice" # stringa
+attivo = True  # boleano
+lista = [1,2,3,"Ciao"] # lista
+lista[3] = "Quattro" # si modifica il singolo elemento
+print(lista)
+coordinate = (10, 20) # tupla
+coordinate = (30, 10, 78) # occore sovarscrivere la tupla per modificare
+print(coordinate[0])
+utente = {"nome": "Alice", "eta": 35, "altezza": "175cm"}
+print(utente["nome"])
