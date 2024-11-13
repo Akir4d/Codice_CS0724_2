753330c26a0afeb0bed77853f05a6c665a5cbb28
Paolo Rampino <liadserv@gmail.com>
2024-11-13 04:43:53 -0500

    up

---
+++ stringhe.py
@@ -0,0 +1,11 @@
+x="questa e' una stringa" # sia doppi apici
+y='questa un altra' # e sia apici singoli
+
+n="Hello, World"
+print(n.split(sep=","))
+print(n.upper())
+print(n.lower())
+f= "This is Python!"
+print(n + "! " + f)
+print(b"Ciao")
+if(f.endswith("!")): print("Vero")
