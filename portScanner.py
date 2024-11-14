a73200622621c23f7b21fcb8b34ba7d4ccce348e
Paolo Rampino <liadserv@gmail.com>
2024-11-14 05:55:38 -0500

    up

--- portScanner.py
+++ portScanner.py
@@ -10,6 +10,7 @@ print("Scansione in corso di", target, "dalla porta", lport, "alla porta", hport
 chiuse = ""
 for port in range(lport, hport+1):
     s = so.socket(so.AF_INET, so.SOCK_STREAM)
+    s.settimeout(0.01)
     status = s.connect_ex((target, port))
     if(status == 0):
         print('*** Porta', port, '- APERTA ***')
